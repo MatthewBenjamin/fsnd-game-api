3c78bbfea9c7e961e718c0adcc8b05b4dbfe68cd
Matthew Benjamin <matt.g.benjamin@gmail.com>
2016-05-05 23:09:23 -0400

    feat: add graceful error handling to get_by_urlsafe

--- utils.py
+++ utils.py
@@ -1,9 +1,22 @@
 # utils.py - General use functions
 
+import endpoints
 from google.appengine.ext import ndb
 
-# TODO: add error handling
 def get_by_urlsafe(urlsafe_key, model):
-    value = ndb.Key(urlsafe=urlsafe_key).get()
-    if isinstance(value, model):
-        return value
+    try:
+        key = ndb.Key(urlsafe=urlsafe_key)
+    except TypeError:
+        raise endpoints.BadRequestException('Invalid Key')
+    except Exception, e:
+        if e.__class__.__name__ == 'ProtocolBufferDecodeError':
+            raise endpoints.BadRequestException('Invalid Key')
+        else:
+            raise
+
+    entity = key.get()
+    if not entity:
+        return None
+    if not isinstance(entity, model):
+        raise ValueError('Incorrect Kind')
+    return entity
